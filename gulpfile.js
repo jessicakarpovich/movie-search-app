45c3172f9f3a19d1a3f1fa21d5df73347d7a1d7b
Jessica Karpovich <karpovichjm@gmail.com>
2018-01-05 20:40:00 -0800

    Added Browersync task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
-// Includes
+// Requires
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+// Create method required by Browsersync
+var browserSync = require('browser-sync').create();
 
 gulp.task('scss', function() {
     return gulp.src('./scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./css'))
+        
+        // Make sure Browsersync runs after the css is finished compiling
+        .pipe(browserSync.reload({
+            stream:true
+    }))
 })
 
-gulp.task('watch', function() {
-    gulp.watch('./scss/*.scss', ['scss'])
+// Initiallize Browsersync
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+})
+
+gulp.task('watch', ['browser-sync', 'scss'], function() {
+    // glob watches for files and files inside folders
+    gulp.watch('./scss/**/*.scss', ['scss'])
 })
